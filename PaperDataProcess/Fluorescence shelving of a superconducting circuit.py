652faffce57fd2c21565031461021e3ab7ceefd2
xionghn13 <xionghn13@gmail.com>
2019-12-17 14:16:44 -0500

    Update Fluorescence shelving of a superconducting circuit.py

--- PaperDataProcess/Fluorescence shelving of a superconducting circuit.py
+++ PaperDataProcess/Fluorescence shelving of a superconducting circuit.py
@@ -1,6 +1,5 @@
 from QubitDataProcessPackages import *
 
-
 # figure 2
 
 DataPath = 'C:\SC Lab\Projects\Fluxonium\data_process/paper_figures\Fluorescence/'
@@ -24,7 +23,6 @@ BackPower = BackPower[0, 0]
 BackFreq = BackFreq[:, 0]
 BackComplex = BackComplex[:, 0]
 BackPowerStr = str(BackPower)
-
 
 [OneFreq, OnePower, OneComplex] = edf.readFPSweepLabber(DataPath + OneToneFile)
 OnePowerUniq = np.unique(OnePower)
@@ -57,7 +55,6 @@ NumPowerTrunc = len(OnePowerUniqTrunc)
 OneComplexNormalizedTrunc = OneComplexNormalized[FreqInd, :]
 OneComplexNormalizedTrunc = OneComplexNormalizedTrunc[:, PowerInd]
 
-
 BackFreqUniq = np.unique(BackFreq)
 BackFreqInd = (EndFreq > BackFreqUniq) == (BackFreqUniq > StartFreq)
 BackFreqUniqTrunc = BackFreqUniq[BackFreqInd]
@@ -75,205 +72,90 @@ f.close()
 
 # figure 3
 
-if Calibration:
-    if BackgroundFile == []:
-        [Plus50MHzBackFreq, Plus50MHzBackComplex] = edf.readFSweepDat(DataPath + Plus50MHzBackgroundFile)
-        Plus50MHzBackPowerStr = Plus50MHzBackgroundFile.split('_')[5][:-3]
-        Plus50MHzBackPower = float(Plus50MHzBackPowerStr)
+BackgroundFile = 'RefRabiCal_4.hdf5'
+RabiFile = 'RefRabi_12.hdf5'
+PopulationConversionConst = [1, 1.]
+# use rabi to calibrate rabi. Plus or minus are merged in one array.
+[BackFreq, BackPower, BackComplex] = edf.readRefRabiCalLabber(BackgroundFolder + BackgroundFile)
+Plus50MHzBackPower = BackPower
+Minus50MHzBackPower = BackPower
+Plus50MHzBackFreq = BackFreq
+Plus50MHzBackComplex = BackComplex
+Minus50MHzBackFreq = Plus50MHzBackFreq
+Minus50MHzBackComplex = Plus50MHzBackComplex
+# read data
+ReadoutPower = edf.readReadoutPowerLabber(DataPath + RabiFile)
+ReadoutFreq = edf.readReadoutFreqLabber(DataPath + RabiFile)
+MeasurementType = 'rabi'
+[Time, ReadoutLowerFreq, ReadoutHigherFreq, ComplexLowerFreq, ComplexHigherFreq] = edf.readRefRabiLabber(
+    DataPath + RabiFile)
+RComplexLowerFreq = sbf.FPSweepBackgroundCalibrate(ReadoutLowerFreq, ReadoutPower, ComplexLowerFreq,
+                                                   Plus50MHzBackFreq,
+                                                   Plus50MHzBackComplex, Plus50MHzBackPower)
+RComplexHigherFreq = sbf.FPSweepBackgroundCalibrate(ReadoutHigherFreq, ReadoutPower, ComplexHigherFreq,
+                                                    Minus50MHzBackFreq,
+                                                    Minus50MHzBackComplex, Minus50MHzBackPower)
+ComplexLowerFreqNormalized = ComplexLowerFreq * 10 ** (- ReadoutPower / 20)
+ComplexHigherFreqNormalized = ComplexHigherFreq * 10 ** (- ReadoutPower / 20)
 
-        [Minus50MHzBackFreq, Minus50MHzBackComplex] = edf.readFSweepDat(DataPath + Minus50MHzBackgroundFile)
-        Minus50MHzBackPowerStr = Minus50MHzBackgroundFile.split('_')[5][:-3]
-        Minus50MHzBackPower = float(Minus50MHzBackPowerStr)
-    elif BackgroundFile.startswith('one_tone'):
-        [Plus50MHzBackFreq, Plus50MHzBackComplex] = edf.readFSweepDat(BackgroundFolder + BackgroundFile)
-        Plus50MHzBackPowerStr = BackgroundFile.split('_')[5][:-3]
-        Plus50MHzBackPower = float(Plus50MHzBackPowerStr)
+y_data = np.array(RComplexLowerFreq.real, dtype='float64')
+population = (PopulationConversionConst[0] - y_data) * PopulationConversionConst[1]
+f = h5py.File(DataPath + 'rabi_data.hdf5', 'w')
+yset = f.create_dataset('y', data=population)
+xset = f.create_dataset('x', data=Time)
+f.close()
 
-        [Minus50MHzBackFreq, Minus50MHzBackComplex] = [Plus50MHzBackFreq, Plus50MHzBackComplex]
-        Minus50MHzBackPowerStr = Plus50MHzBackPowerStr
-        Minus50MHzBackPower = Plus50MHzBackPower
-    elif BackgroundFile.endswith('dat'):
-        BackgroundFileStrList = BackgroundFile.split('_')
-        MeasurementType = BackgroundFileStrList[1]
-        if MeasurementType == 'rabi' and BackgroundFileStrList[3] != 'no pump':
-            MeasurementType = 'pump rabi'
-        BackFreqDict = {'t1': 3, 'rabi': 5, 'pump rabi': 6}
-        BackFreqInd = BackFreqDict[MeasurementType]
-        BackPowerDict = {'t1': 5, 'rabi': 7, 'pump rabi': 8}
-        BackPowerInd = BackPowerDict[MeasurementType]
-        BackPower = float(BackgroundFileStrList[BackPowerInd][:-3])
-        BackLowerFreq = round(float(BackgroundFileStrList[BackFreqInd][:-3]) - IQModFreq, 4)
-        BackHigherFreq = round(BackLowerFreq + IQModFreq * 2, 15)
-        BackPower = float(BackgroundFileStrList[BackPowerInd][:-3])
-        Plus50MHzBackPower = BackPower
-        Minus50MHzBackPower = BackPower
-        if MeasurementType in ('rabi', 'transient'):
-            [Time, ComplexLowerFreq, ComplexHigherFreq] = edf.readRabiH5(BackgroundFolder + BackgroundFile)
-        elif MeasurementType == 't1':
-            [Time, ComplexLowerFreq, ComplexHigherFreq] = edf.readT1H5(BackgroundFolder + BackgroundFile)
-        Plus50MHzBackFreq = np.array([BackLowerFreq, BackHigherFreq])
-        Plus50MHzBackComplex = np.array([ComplexLowerFreq.mean(), ComplexHigherFreq.mean()])
-        Minus50MHzBackFreq = Plus50MHzBackFreq
-        Minus50MHzBackComplex = Plus50MHzBackComplex
-    elif BackgroundFile.endswith('hdf5'):
-        if BackgroundFile.startswith('transient'):
-            [Time, ComplexLowerFreq] = edf.readRabiLabber(BackgroundFolder + BackgroundFile)
-            BackPower = edf.readReadoutPowerLabber(BackgroundFolder + BackgroundFile)
-            Plus50MHzBackPower = BackPower
-            Minus50MHzBackPower = BackPower
-            BackFreq = edf.readReadoutFreqLabber(BackgroundFolder + BackgroundFile)
-            Plus50MHzBackFreq = np.array([BackFreq])
-            Plus50MHzBackComplex = np.array([ComplexLowerFreq.mean()])
-            Minus50MHzBackFreq = Plus50MHzBackFreq
-            Minus50MHzBackComplex = Plus50MHzBackComplex
-        elif BackgroundFile.startswith('calibration') or BackgroundFile.startswith('power'):
-            [BackFreq, BackComplex] = edf.readFSweepLabber(BackgroundFolder + BackgroundFile)
-            BackPower = edf.readReadoutPowerLabber(BackgroundFolder + BackgroundFile)
-            Plus50MHzBackPower = BackPower
-            Minus50MHzBackPower = BackPower
-            Plus50MHzBackFreq = BackFreq
-            Plus50MHzBackComplex = BackComplex
-            Minus50MHzBackFreq = Plus50MHzBackFreq
-            Minus50MHzBackComplex = Plus50MHzBackComplex
-        elif BackgroundFile.startswith('RefRabiCal'):
-            # use rabi to calibrate rabi. Plus or minus are merged in one array.
-            [BackFreq, BackPower, BackComplex] = edf.readRefRabiCalLabber(BackgroundFolder + BackgroundFile)
-            Plus50MHzBackPower = BackPower
-            Minus50MHzBackPower = BackPower
-            Plus50MHzBackFreq = BackFreq
-            Plus50MHzBackComplex = BackComplex
-            Minus50MHzBackFreq = Plus50MHzBackFreq
-            Minus50MHzBackComplex = Plus50MHzBackComplex
+# t1 t2
+RabiFileList = [
+    'ref_t1_ramsey_echo_interleaved_1.hdf5',
+]
 
-# read data file
-if RabiFile.endswith('dat'):
-    RabiFileStrList = RabiFile.split('_')
-    MeasurementType = RabiFileStrList[1]
-    if MeasurementType == 'rabi' and RabiFileStrList[3] != 'no pump':
-        MeasurementType = 'pump rabi'
-    if MeasurementType == 'Ch1':
-        MeasurementType = 'Ch1 rabi'
-    if MeasurementType == 'Ch2 rabi 2Vpp':
-        MeasurementType = 'Ch1 pump rabi'
+NumFile = len(RabiFileList)
+CounterArray = np.zeros([NumFile, ])
+# analyze background file
 
-    ReadoutFreqDict = {'t1': 3, 'rabi': 5, 'Ch1 rabi': 6, 'pump rabi': 6, 'Ch1 pump rabi': 4, 't2': 3, 't2echo': 3}
-    ReadoutFreqInd = ReadoutFreqDict[MeasurementType]
-    ReadoutPowerDict = {'t1': 5, 'rabi': 7, 'Ch1 rabi': 8, 'pump rabi': 8, 'Ch1 pump rabi': 6, 't2': 5, 't2echo': 5}
-    ReadoutPowerInd = ReadoutPowerDict[MeasurementType]
-    ReadoutFreq = round(float(RabiFileStrList[ReadoutFreqInd][:-3]), 4)
-    ReadoutLowerFreq = round(float(RabiFileStrList[ReadoutFreqInd][:-3]) - IQModFreq, 4)
-    ReadoutHigherFreq = round(ReadoutLowerFreq + IQModFreq * 2, 15)
-    ReadoutPower = float(RabiFileStrList[ReadoutPowerInd][:-3])
-    QubitFreqDict = {'rabi': 8, 'Ch1 rabi': 9, 't1': 6, 'Ch1 pump rabi': 7, 't2': 6, 't2echo': 7}
-    QubitFreqInd = QubitFreqDict[MeasurementType]
-    QubitFreq = float(RabiFileStrList[QubitFreqInd][5:-3])
-    if QubitFreq in (ReadoutLowerFreq, ReadoutHigherFreq) and MeasurementType == 'rabi':
-        MeasurementType = 'transient'
+RabiFile = 'ref_t1_ramsey_echo_interleaved_1.hdf5'
+RabiFileStrList = RabiFile[:-5].split('_')
+MeasurementType = RabiFileStrList[0]
+MeasurementType = 'ref_t1_ramsey_echo_interleaved'
+ReadoutFreq = edf.readReadoutFreqLabber(DataPath + RabiFile)
+ReadoutPower = edf.readReadoutPowerLabber(DataPath + RabiFile)
+QubitFreq = edf.readPumpFreqLabber(DataPath + RabiFile)
 
-    if MeasurementType in ('rabi', 'Ch1 rabi', 'transient', 'Ch1 pump rabi'):
-        [Time, ComplexLowerFreq, ComplexHigherFreq] = edf.readRabiH5(DataPath + RabiFile)
-        if LimitTimeRange:
-            TimeInd = (EndTime >= Time) == (Time >= StartTime)
-            Time = Time[TimeInd]
-            ComplexLowerFreq = ComplexLowerFreq[TimeInd]
-            ComplexHigherFreq = ComplexHigherFreq[TimeInd]
-    elif MeasurementType == 't1':
-        [Time, ComplexLowerFreq, ComplexHigherFreq] = edf.readT1H5(DataPath + RabiFile)
-        if LimitTimeRange:
-            TimeInd = (EndTime >= Time) == (Time >= StartTime)
-            Time = Time[TimeInd]
-            ComplexLowerFreq = ComplexLowerFreq[TimeInd]
-            ComplexHigherFreq = ComplexHigherFreq[TimeInd]
-    elif MeasurementType in ('t2', 't2echo'):
-        [Time, Complex] = edf.readT2H5(DataPath + RabiFile)
-        if LimitTimeRange:
-            TimeInd = (EndTime >= Time) == (Time >= StartTime)
-            Time = Time[TimeInd]
-            Complex = Complex[TimeInd]
-elif RabiFile.endswith('hdf5'):
-    ReadoutPower = edf.readReadoutPowerLabber(DataPath + RabiFile)
-    ReadoutFreq = edf.readReadoutFreqLabber(DataPath + RabiFile)
-    if RabiFile.startswith('transient'):
-        MeasurementType = 'transient no ref'
-        [Time, Complex] = edf.readRabiLabber(DataPath + RabiFile)
-    elif RabiFile.startswith('rabi'):
-        MeasurementType = 'rabi no ref'
-        if RabiFile.startswith('rabi CH1 drive'):
-            [Time, Complex] = edf.readRabiCH1DriveLabber(DataPath + RabiFile)
-            # [Time, Complex] = edf.readRabiLabber(DataPath + RabiFile)
-        elif RabiFile.startswith('rabi CH1 pumped'):
-            [Time, Complex] = edf.readRabiCH1PumpedLabber(DataPath + RabiFile)
-        else:
-            [Time, Complex] = edf.readRabiLabber(DataPath + RabiFile)
-    elif RabiFile.startswith('int_t1'):
-        MeasurementType = 't1 no ref'
-        [Time, Complex] = edf.readIntegratedT1Labber(DataPath + RabiFile)
-    elif RabiFile.startswith('t1'):
-        MeasurementType = 't1 no ref'
-        [Time, Complex] = edf.readT1Labber(DataPath + RabiFile)
-    elif RabiFile.startswith('t2_ramsey'):
-        MeasurementType = 't2'
-        [Time, Complex] = edf.readT2Labber(DataPath + RabiFile)
-    elif RabiFile.startswith('t2_echo'):
-        MeasurementType = 't2echo'
-        [Time, Complex] = edf.readT2Labber(DataPath + RabiFile)
-    elif RabiFile.startswith('RefRabi'):
-        MeasurementType = 'rabi'
-        [Time, ReadoutLowerFreq, ReadoutHigherFreq, ComplexLowerFreq, ComplexHigherFreq] = edf.readRefRabiLabber(
-            DataPath + RabiFile)
+[Time, Counter, ReadoutFreq, ComplexVoltageT1, ComplexVoltageRamsey,
+ ComplexVoltageEcho] = edf.readReferencedRepeatedT1RamseyEchoInterleavedSweepLabber(
+    DataPath + RabiFile)
 
-    if LimitTimeRange:
-        TimeInd = (EndTime >= Time) == (Time >= StartTime)
-        Time = Time[TimeInd]
-        if RabiFile.startswith('RefRabi'):
-            ComplexLowerFreq = ComplexLowerFreq[TimeInd]
-            ComplexHigherFreq = ComplexHigherFreq[TimeInd]
-        else:
-            Complex = Complex[TimeInd]
+ComplexVoltageT1Normalized = ComplexVoltageT1 * 10 ** (- ReadoutPower / 20)
+ComplexVoltageRamseyNormalized = ComplexVoltageRamsey * 10 ** (- ReadoutPower / 20)
+ComplexVoltageEchoNormalized = ComplexVoltageEcho * 10 ** (- ReadoutPower / 20)
+RComplexT1 = sbf.FPSweepBackgroundCalibrate(ReadoutFreq, ReadoutPower, ComplexVoltageT1, BackFreq,
+                                            BackComplex, BackPower)
+RComplexRamsey = sbf.FPSweepBackgroundCalibrate(ReadoutFreq, ReadoutPower, ComplexVoltageRamsey,
+                                                BackFreq, BackComplex, BackPower)
+RComplexEcho = sbf.FPSweepBackgroundCalibrate(ReadoutFreq, ReadoutPower, ComplexVoltageEcho,
+                                              BackFreq, BackComplex, BackPower)
 
-if MeasurementType in ('t2', 't2echo', 'transient no ref', 't1 no ref', 'rabi no ref'):
-    ComplexNormalized = Complex * 10 ** (- ReadoutPower / 20)
-    if Calibration:
-        RComplex = sbf.FPSweepBackgroundCalibrate(ReadoutFreq, ReadoutPower, Complex, Plus50MHzBackFreq,
-                                                  Plus50MHzBackComplex, Plus50MHzBackPower)
-    else:
-        RComplex = ComplexNormalized
+y_dataList = [np.array(RComplexT1[:, 0, 0].real, dtype='float64'),
+              np.array(RComplexRamsey[:, 0, 0].real, dtype='float64'),
+              np.array(RComplexEcho[:, 0, 0].real, dtype='float64')]
+for i in range(3):
+    y_dataList[i] = (PopulationConversionConst[0] - y_dataList[i]) * PopulationConversionConst[1]
+x_data = np.array(Time, dtype='float64')
 
-    if RotateComplex:
-        RComplex = AutoRotate(RComplex)
-    y_data = np.array(RComplex.real, dtype='float64')
-    # y_data = np.array(RComplex.imag, dtype='float64')
-
-else:
-    RComplexLowerFreq = sbf.FPSweepBackgroundCalibrate(ReadoutLowerFreq, ReadoutPower, ComplexLowerFreq,
-                                                       Plus50MHzBackFreq,
-                                                       Plus50MHzBackComplex, Plus50MHzBackPower)
-    RComplexHigherFreq = sbf.FPSweepBackgroundCalibrate(ReadoutHigherFreq, ReadoutPower, ComplexHigherFreq,
-                                                        Minus50MHzBackFreq,
-                                                        Minus50MHzBackComplex, Minus50MHzBackPower)
-    ComplexLowerFreqNormalized = ComplexLowerFreq * 10 ** (- ReadoutPower / 20)
-    ComplexHigherFreqNormalized = ComplexHigherFreq * 10 ** (- ReadoutPower / 20)
-
-    [amp_cor_re_fit, amp_cor_im_fit, P0_fit, P0_im_fit] = CorrectionParam
-    if CircleCorrection:
-        RComplexLowerFreq /= (amp_cor_re_fit + amp_cor_im_fit * 1j)
-        RComplexLowerFreq = (RComplexLowerFreq - 1) / (P0_fit + P0_im_fit * 1j) * np.abs(
-            P0_fit + P0_im_fit * 1j) + 1
-
-    # y_data = np.array(RComplexLowerFreq.real, dtype='float64')
-    if FitCorrectedR:
-        y_data = np.array(np.real(RComplexLowerFreq / RComplexHigherFreq), dtype='float64')
-    else:
-        y_data = np.array(RComplexLowerFreq.real, dtype='float64')
+f = h5py.File(DataPath + 't1_ramsey_echo_data.hdf5', 'w')
+yset = f.create_dataset('y', data=y_dataList)
+xset = f.create_dataset('x', data=x_data)
+f.close()
 
 # figure 4
 
-# DataPath = 'C:\SC Lab\Projects\Fluxonium\data_process/ziggy4/'
-# BackgroundFile = 'power spectroscopy_101.hdf5'
-# RabiFileList = [
-#     'transient_28.hdf5',
-#     'transient_27.hdf5',
-# ]
+BackgroundFile = 'power spectroscopy_101.hdf5'
+RabiFileList = [
+    'transient_29.hdf5',
+    'transient_28.hdf5',
+    'transient_27.hdf5',
+]
 #
 # IQModFreq = 0.05
 # # FitForGamma = True
@@ -320,26 +202,8 @@ else:
 #     else:
 #         RComplex = ComplexRabiNormalized
 
-# Fig.2
-# BackgroundFile = 'power spectroscopy_76.hdf5'
-# OneToneFile = 'power spectroscopy_77.hdf5'
+
 #
-# Fig.3
-# BackgroundFile = 'RefRabiCal_4.hdf5'
-# RabiFile = 'RefRabi_12.hdf5'
-#
-# BackgroundFile = 'RefRabiCal_4.hdf5'
-# RabiFileList = [
-# 'ref_t1_ramsey_echo_interleaved_1.hdf5',
-# ]
-#
-#
-# Fig.4
-# BackgroundFile = 'power spectroscopy_101.hdf5'
-# RabiFileList = [
-# 'transient_28.hdf5',
-# 'transient_27.hdf5',
-# ]
 #
 #
 #
